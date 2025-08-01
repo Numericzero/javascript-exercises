cfb36d0e677657310f987a0927644640a1512294
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-01 18:17:46 -0400

    Create function that reverses a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    buff = str.split("")
+    reversed = buff.reduce((string, item) => string = item + string, "")
 
+    return reversed
 };
 
 // Do not edit below this line

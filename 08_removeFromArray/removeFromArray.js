52d80aaa2603fa5aa13ac3aeb4905023312dc9d0
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-01 23:12:01 -0400

    Create function to delete array elements

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    args.forEach(element => {
+        let elemFound = true
+
+        while(elemFound)
+        {
+            let index = arr.indexOf(element)
+            if(index == -1)
+                elemFound = false
+            else
+                arr.splice(index, 1)
+        }
+    });
+
+    return arr
 };
 
 // Do not edit below this line

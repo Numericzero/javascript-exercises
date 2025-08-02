e34035da7f76befc292151da03bb6c4a13a4ad27
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-01 23:29:29 -0400

    Create function to sum values from a to b

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0
+    if(a < 0 || b < 0)
+        return "ERROR"
 
+    if(isNaN(a) || isNaN(b) || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR"
+
+    if(a > b)
+    {
+        let c = a
+        a = b
+        b = c
+    }
+
+    for(let i = a; i <= b; i++)
+    {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line

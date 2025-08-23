e0f42073480d4fd5a12895d64aaca488301fa39d
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-22 23:23:37 -0400

    Modify function to pass all tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let prevValue = 0;
+    let currValue = 1;
 
+    if(n==0)
+        return 0;
+    else if(n<0)
+        return "OOPS"
+
+    for(let i = 1; i < n; i++)
+    {
+        let temp = currValue;
+        currValue = currValue + prevValue;
+        prevValue = temp;
+    }
+
+    return currValue;
 };
 
 // Do not edit below this line

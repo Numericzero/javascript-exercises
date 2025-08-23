d5b3ba8498175e9df4b4ef085c5088ec09789510
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-22 20:45:55 -0400

    Modify function to pass all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, item) => prod *= item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let prod = 1;
+  if(a==0)
+    return prod;
+
+	for(let i=1; i <= a; i++)
+    prod *= i;
+
+  return prod;
 };
 
 // Do not edit below this line

0a3099f0920aff2352ff5d96713daa2e1c89b66e
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-22 23:09:11 -0400

    Modify function to pass all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let oldStr = str.replace(/\s/g, "").replace(/[^\w]/g, "").toLowerCase();
+    let newStr = oldStr.split("").reverse().join("").toLowerCase();
+    return newStr == oldStr;
 };
 
 // Do not edit below this line

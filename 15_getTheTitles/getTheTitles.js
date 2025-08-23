2288cb72f4dd638b479dda1235b3f4840ea1d5bb
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-23 00:02:28 -0400

    Modify function to pass all tests

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = [];
+    for(let item in arr)
+        titles.push(arr[item].title);
 
+    return titles;
 };
 
 // Do not edit below this line

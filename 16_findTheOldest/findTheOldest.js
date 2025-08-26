edaf3b0f6e61a15b9a2307e27a7e8fee4e89fe60
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-26 17:30:23 -0400

    Modify function to pass all tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let sortedArr = arr.sort((a, b) => {
+        let d = new Date()
+        let curYear = d.getFullYear();
+        let aage = null;
+        let bage = null;
+        if(a.yearOfDeath)
+            aage = a.yearOfDeath - a.yearOfBirth;
+        else
+            aage = curYear - a.yearOfBirth;
 
+        if(b.yearOfDeath)
+            bage = b.yearOfDeath - b.yearOfBirth;
+        else
+            bage = curYear - b.yearOfBirth;
+
+        return aage-bage;
+    }).reverse();
+    return sortedArr[0];
 };
 
 // Do not edit below this line
